f2647836dd264a55be9e6cb14675c62e6b93689b
Adam <alsomervell@gmail.com>
2024-05-31 09:59:47 -0400

    adjustments

---
+++ pages/course.vue
@@ -0,0 +1,63 @@
+<template>
+    <div
+      class="p-12 bg-green-300 w-full h-full min-h-screen flex flex-col items-center"
+    >
+      <div class="prose mb-12">
+        <h1>
+          <span class="font-medium text-green-800">
+            Course:
+            <span class="font-bold text-green-800">Mastering Nuxt 3</span>
+          </span>
+        </h1>
+      </div>
+  
+      <div class="flex flex-row justify-center flex-grow">
+        <div
+          class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] max-w-[30ch] flex flex-col"
+        >
+          
+          <h3 class="text-green-800"> Chapters </h3>
+          <!--Renders the chapters into navigation tab-->
+          <div
+          class="space-y-1 mb-4 flex flex-col"
+          v-for="chapter in chapters"
+          :key="chapter.slug"
+          >
+          <h4>{{ chapter.title }}</h4>
+          
+          <!--Shows each lesson within a chapter-->
+          <!--Provides a lesson path route to navigate to each lesson and highlights current selection-->
+          <NuxtLink
+            v-for="(lesson, index) in chapter.lessons"
+            :key="lesson.slug"
+            class="flex flex-row space-x-1 no-underline prose-sm font-normal py-1 px-4 -mx-4"
+            
+            :to="lesson.path"
+            :class="{
+              'text-blue-500':
+                lesson.path === $route.fullPath,
+              'text-gray-600':
+                lesson.path !== $route.fullPath,
+            }"
+          >
+          <!--Numerates the lessons and renders title-->
+          <span class="text-gray-500"
+              >{{ index + 1 }}.
+          </span>
+          <span>{{ lesson.title }}</span>
+          
+          </NuxtLink>
+          </div>
+        </div>
+  
+        <div class="prose p-12 bg-white rounded-md w-[65ch]">
+          <NuxtPage />
+        </div>
+      </div>
+    </div>
+  </template>
+
+<script setup>
+const { chapters } = useCourse();
+</script>
+
