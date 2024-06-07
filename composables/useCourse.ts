b56ee04379cd81dc9fcd806b97b2f896ea6aff69
Adam <alsomervell@gmail.com>
2024-06-07 17:03:47 -0400

    test-deploy

--- composables/useCourse.ts
+++ composables/useCourse.ts
@@ -1,4 +1,4 @@
-import { CourseOutline } from '~~/server/api/course/meta.get';
+import type { CourseOutline } from '~~/server/api/course/meta.get';
 
 export default async () =>
   useFetchWithCache<CourseOutline>('/api/course/meta');
