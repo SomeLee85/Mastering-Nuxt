33268a1097060d15ae3180b52dadb5c1ee22069f
Adam <alsomervell@gmail.com>
2024-06-10 12:34:27 -0400

    7-4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
+import vsharp from 'vite-plugin-vsharp';
+
 export default defineNuxtConfig({
   nitro: {
     prerender: {
       routes: ['/landing'],
     },
+  },
+
+  vite: {
+    plugins: [vsharp()],
   },
 
   modules: [
