dc56553bce4b837f5259a5578d4adb7dbd6d6848
Adam <alsomervell@gmail.com>
2024-06-11 18:00:15 -0400

    changed landing to index

---
+++ pages/example.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 class="text-green-800 text-bold">Quick Links</h1>
+  <span>
+    <NuxtLink
+      class="underline font-medium"
+      :href="firstLesson.path"
+    >
+      Go to first lesson.
+    </NuxtLink>
+  </span>
+</template>
+
+<style scoped>
+span:hover {
+  color: #008bff;
+}
+</style>
+
+<script setup lang="ts">
+const firstLesson = await useFirstLesson();
+</script>
