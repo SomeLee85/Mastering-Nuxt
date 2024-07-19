787cca8a356db14a38948bfee1abca56f3f8bb26
Adam <alsomervell@gmail.com>
2024-07-19 17:58:05 -0400

    New Features

--- components/DropDownPug.vue
+++ components/DropDownPug.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
-    <button @click="courseIsOpen = !courseIsOpen" class="px-1 py-1 rounded hover:bg-slate-600">Course</button>
-    <div v-if="courseIsOpen" class="absolute left-21 mt-1 px-2 py-2 rounded-lg shadow-xl bg-slate-600">
+    <button @click="courseIsOpen = !courseIsOpen" class="px-1 py-1 rounded">
+      Course
+      <p class="float-right" style="transform: translate(10px, -4px)">⌄</p>
+    </button>
+    <div v-if="courseIsOpen" class="absolute items-center mt-1 px-2 py-2 rounded-lg shadow-xl bg-sky-800">
       <div class="mt-1 block py-2 rounded">
-        <button @click="chOneOpened = !chOneOpened" class="hover:underline px-3 text-lg rounded">1-chapter-1</button>
-        <div v-if="chOneOpened" class="bg-gray-500 rounded">
+        <button @click="chOneOpened = !chOneOpened" class="hover:underline px-3 text-lg rounded">
+          1-chapter-1
+          <p v-if="!chOneOpened" class="float-right text-xs" style="transform: translate(5px, 7px)">﹥</p>
+          <p v-else class="float-right text-xl" style="transform: translate(6px, 4px)">˅</p>
+        </button>
+        <div v-if="chOneOpened">
           <a
             class="mt-1 block px-4 py-1 rounded hover:bg-teal-600 text-sm"
             href="/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3"
@@ -23,8 +30,12 @@
         </div>
       </div>
       <div v-if="user.paid">
-        <button @click="chTwoOpened = !chTwoOpened" class="hover:underline px-3 text-lg rounded">2-chapter-2</button>
-        <div v-if="chTwoOpened" class="bg-gray-500 rounded">
+        <button @click="chTwoOpened = !chTwoOpened" class="hover:underline px-3 text-lg rounded">
+          2-chapter-2
+          <p v-if="!chTwoOpened" class="float-right text-xs" style="transform: translate(5px, 7px)">﹥</p>
+          <p v-else class="float-right text-xl" style="transform: translate(6px, 4px)">˅</p>
+        </button>
+        <div v-if="chTwoOpened">
           <a
             class="mt-1 block px-4 py-1 rounded hover:bg-teal-600 text-sm"
             href="/course/chapter/2-chapter-2/lesson/1-using-typescript-with-the-options-api-in-components"
@@ -48,8 +59,10 @@
         </div>
         <button @click="chThreeOpened = !chThreeOpened" class="mt-1 block px-3 py-2 rounded hover:underline text-lg">
           3-chapter-3
+          <p v-if="!chThreeOpened" class="float-right text-xs" style="transform: translate(5px, 7px)">﹥</p>
+          <p v-else class="float-right text-xl" style="transform: translate(6px, 4px)">˅</p>
         </button>
-        <div v-if="chThreeOpened" class="bg-gray-500 rounded">
+        <div v-if="chThreeOpened">
           <a
             class="mt-1 block px-4 py-1 rounded hover:bg-teal-600 text-sm"
             href="/course/chapter/3-chapter-3/lesson/1-using-typescript-with-the-options-api-in-components"
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="w-full text-white bg-gray-700" style="overflow: hidden; display: flex; align-items: center">
-    <a class="hover:bg-gray-600 rounded" href="/" style="padding: 23px">
-      <img :src="homeIcon" style="width: 30px; height: 25px; float: left; transform: translate(-5px, -4px)" />
+  <div class="w-full text-white bg-blue-900" style="overflow: hidden; display: flex; align-items: center">
+    <a class="hover:bg-sky-800 rounded" href="/" style="padding: 23px">
+      <img :src="homeIcon" style="width: 30px; height: 27px; float: left; transform: translate(-8px, -5px)" />
       Home
     </a>
-    <p class="hover:bg-gray-600 rounded" style="padding: 20px">
+    <p class="hover:bg-sky-800 rounded" style="padding: 21px">
       <DropDownPug />
     </p>
-    <a class="hover:bg-gray-600 rounded" href="/FAQ" style="padding: 23px">FAQs</a
-    ><a class="hover:bg-gray-600 rounded" href="/about" style="padding: 23px">About</a>
+    <a class="hover:bg-sky-800 rounded" href="/FAQ" style="padding: 25px">FAQs</a
+    ><a class="hover:bg-sky-800 rounded" href="/about" style="padding: 25px">About</a>
     <div v-if="user?.isLoggedIn" style="position: absolute; right: 0.2%">
       <UserCard />
     </div>
@@ -20,7 +20,7 @@
 </template>
 
 <script setup lang="ts">
-import homeIcon from '~/assets/images/home-icon.png';
+import homeIcon from '~/assets/images/home-icon-G.png';
 import { useUserStore } from '@/stores/user';
 import { FwbDropdown } from 'flowbite-vue';
 const user = useUserStore();
