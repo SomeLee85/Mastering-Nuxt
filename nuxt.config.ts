14f6f7871c135fc5d9f91df4e9a15510a7b872cf
Adam <alsomervell@gmail.com>
2024-07-22 09:43:04 -0400

    fixing routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,6 @@ export default defineNuxtConfig({
     public: {
       stripeKey: '',
       apiUrl: process.env.API_URL || 'http://localhost:8888',
-      prodApiUrl: process.env.PROD_API_URL || 'https://mastering-nuxt-netlify.netlify.app',
     },
   },
   ssr: false,
